0a7138212de3249fa585bf364d8c2a16e9edf514
hoba-android <heba96@yahoo.com>
2021-06-20 11:50:09 +0200

    sound play

--- src/screens/Learn.js
+++ src/screens/Learn.js
@@ -9,18 +9,44 @@ import {
   FlatList,
 } from 'react-native';
 import Video1 from 'react-native-video';
+import Sound from 'react-native-sound';
+
+import Icon2 from 'react-native-vector-icons/AntDesign';
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
 
 const sampleVid = 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4';
 
 import LearnComponent from './components/LearnComponent';
 import VideoPlayer from './components/VideoPlayer';
 
+const soundUrl =
+  'https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3';
+const soundTitle = 'Play some music';
+
 const Learn = ({navigation}) => {
   const [paused, setPuased] = useState(true);
   const [controls, setContols] = useState(false);
 
-  const seekLeft = () => {};
-  const seekRight = () => {};
+  const mySound = new Sound(soundUrl, Sound.MAIN_BUNDLE, error => {
+    if (error) {
+      console.log('failed to load the sound', error);
+      return;
+    }
+  });
+
+  const playSound = () => {
+    mySound.play(success => {
+      if (success) {
+        console.log('successfully finished playing');
+      } else {
+        console.log('playback failed due to audio decoding errors');
+      }
+    });
+  };
+
+  const puaseSound = () => {
+    mySound.pause();
+  };
 
   useEffect(() => {
     navigation.addListener('focus', () => {
@@ -48,6 +74,25 @@ const Learn = ({navigation}) => {
           style={{width: 360, height: 300, margin: 10, alignSelf: 'center'}}
           controls={controls}
         />
+
+        <View style={{borderWidth: 2, borderRadius: 10, borderColor: 'orange'}}>
+          <Icon name="playlist" size={100} color="black" style={{margin: 20}} />
+        </View>
+
+        <View style={{flexDirection: 'row'}}>
+          <TouchableOpacity onPress={playSound}>
+            <Icon2 name="play" size={40} color="black" style={{margin: 20}} />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={puaseSound}>
+            <Icon2
+              name="pausecircle"
+              size={40}
+              color="black"
+              style={{margin: 20}}
+            />
+          </TouchableOpacity>
+        </View>
       </View>
     </ScrollView>
   );
